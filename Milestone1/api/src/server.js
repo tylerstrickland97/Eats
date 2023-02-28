15be236dde40300907160a54344cedd114655ae7
Tyler Strickland <tstrick2@github.ncsu.edu>
2023-02-27 19:08:48 -0500

    Adding outline for REST APIs

--- Milestone1/api/src/server.js
+++ Milestone1/api/src/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const routes = require('./APIRoutes.js');
+
+
 
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req,  res) => {
   res.json({your_api: 'it works'});
 });
+
 
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
