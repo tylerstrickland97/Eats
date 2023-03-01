7ba3565fbdac747663c5963574d6b84c647d1c20
Tyler Strickland <tstrick2@github.ncsu.edu>
2023-02-28 20:53:39 -0500

    Adding changes to backend

--- Milestone1/api/src/server.js
+++ Milestone1/api/src/server.js
@@ -2,16 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const routes = require('./APIRoutes.js');
-
-
+const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
 
-app.get('/', (req,  res) => {
-  res.json({your_api: 'it works'});
-});
+
 
 
 // As our server to listen for incoming connections
--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT;
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + '/static'));
 
+const html_dir  = __dirname + '/templates/';
+console.log(html_dir);
+
+app.get('/', (req, res) => {
+    res.sendFile(html_dir + 'homepage.html');
+})
+
 
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
