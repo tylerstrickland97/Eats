bbd5fd4184c2cc46d62fd389222bc34a03a55ad0
Sheryl Lee <sheryllee@MacBook-Pro-2.local>
2023-03-02 22:54:06 -0500

    Adding changes to load restaurant data on homepage

--- Milestone1/api/src/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiRouter = require('./apiRoutes')
-
-router.use('/api', apiRouter);
-module.exports = router;
--- Milestone1/api/src/server.js
+++ Milestone1/api/src/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const routes = require('./routes');
+const routes = require('./APIRoutes');
 
 app.use(express.json());
 app.use(routes);
-
-
 
 
 // As our server to listen for incoming connections
