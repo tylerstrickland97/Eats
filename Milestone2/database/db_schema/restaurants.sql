deac864cbfecf775e1763292d8bb0356b427e688
Tyler Strickland <tstrick2@ncsu.edu>
2023-04-02 07:56:49 -0400

    Pushing the db_schema file that initializes our database.

--- Milestone2/database/db_schema/restaurants.sql
+++ Milestone2/database/db_schema/restaurants.sql
@@ -11,7 +11,61 @@
 CREATE TABLE IF NOT EXISTS `restaurants` (
   `restaurant_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `restaurant_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`restaurants_id`)
+  PRIMARY KEY (`restaurant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `password_hash` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `menu_items` (
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `restaurant` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `calories` varchar(100) NOT NULL,
+  `fat_g` varchar(100) NOT NULL,
+  `carbohydrates_g` varchar(100) NOT NULL,
+  `protein_g` varchar(100) NOT NULL,
+  `sodium_mg` varchar(100) NOT NULL,
+  `cholesterol_mg` varchar(100) NOT NULL,
+  `fiber_g` varchar(100) NOT NULL,
+  `restaurant_id` int(10) unsigned NOT NULL
+  PRIMARY KEY (`item_id`),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DELETE FROM `restaurants`;
+INSERT INTO `restaurants` (`restaurant_id`, `restaurant_name`) VALUES
+  (1, 'Starbucks'),
+  (2, 'McDonalds'),
+  (3, 'Burger King'),
+  (4, 'Taco Bell'),
+  (5, 'KFC'),
+  (6, 'Chilis Bar and Grill'),
+  (7, 'Five Guys'),
+  (8, 'Chick-fil-a'),
+  (9, 'Cook Out'),
+  (10, 'Dominos'),
+  (11, 'Golden Corral'),
+  (12, 'Bojangles'),
+  (13, 'Wendys'),
+  (14, 'Olive Garden'),
+  (15, 'Panera Bread'),
+  (16, 'Red Robin'),
+  (17, 'Red Lobster'),
+  (18, 'Subway'),
+  (19, 'Pizza Hut'),
+  (20, 'Carls Jr.'),
+  (21, 'Popeyes'),
+  (22, 'Chipotle');
+
+-- ALTER TABLE `menu_items`
+-- ADD FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants`(`restaurant_id`);
 
