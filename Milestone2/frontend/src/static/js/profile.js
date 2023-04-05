d48b05907989ba4024fa75f7e73719aeb8696c9c
Tyler Strickland <tstrick2@ncsu.edu>
2023-04-05 17:39:00 -0400

    Updating the profile page to reflect a user's favorites

--- Milestone2/frontend/src/static/js/profile.js
+++ Milestone2/frontend/src/static/js/profile.js
@@ -14,9 +14,61 @@ window.onload = () => {
         email.innerHTML = user.email;
         fullname.innerHTML = `${user.first_name} ${user.last_name}`;
         userid.innerHTML = user.username;
+        loadFavorites(user.id);
     }).catch(err => {
         console.log("We are not logged in");
         document.location = '/';
     })
+}
+
+function filterRestaurantName(restaurant_name) {
+    let filtered_name = restaurant_name.replaceAll(/[^A-Za-z\s]/g, '');
+    filtered_name = filtered_name.replaceAll(' ', '-');
+    return filtered_name;
+}
+
+function loadFavorites(userId) {
+    const userFavorites = document.querySelector('.user-favorites');
+    api.getUserFavorites(userId).then(favorites => {
+        if (favorites.length == 0) {
+            let noFavoritesText = document.createElement('p');
+            noFavoritesText.innerHTML = "You currently have no favorites";
+            userFavorites.appendChild(noFavoritesText);
+        }
+        else {
+            favorites.forEach(fav => {
+                api.getRestaurantById(fav.restaurant_id).then(restaurant => {
+                    fillFavoriteHTML(restaurant);
+                })
+            })
+        }
+
+    })
+
+}
+
+function fillFavoriteHTML(restaurant) {
+    const userFavorites = document.querySelector('.user-favorites');
+
+    let favoriteContainer = document.createElement('div');
+    favoriteContainer.classList.add('account-information');
+    favoriteContainer.classList.add('favorite-container');
+
+    let favoriteLogoImg = document.createElement('img');
+    favoriteLogoImg.src = `imgs/restaurant-logos/${filterRestaurantName(restaurant.name)}-logo.png`;
+
+    let nameDiv = document.createElement('div');
+    nameDiv.classList.add('favorite-name');
+
+    let nameLink = document.createElement('a');
+    nameLink.innerHTML = restaurant.name;
+    nameLink.href = '/restaurant?id=' + restaurant.id;
+
+    nameDiv.appendChild(nameLink);
+
+    favoriteContainer.appendChild(favoriteLogoImg);
+    favoriteContainer.appendChild(nameDiv);
+
+    userFavorites.appendChild(favoriteContainer);
 }
 
